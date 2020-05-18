ab027b7e745cf6fe91ef6438b8ff4f69da23d629
zhen <zhen.yang@hotmail.fr>
2020-05-18 12:21:39 +0200

    first

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StoreService } from '../myservice/store.service';
+import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+	allvideos:any = [];
+	allvideos2:any = [];
+  constructor(private ss : StoreService, private sanitizer : DomSanitizer) {
+  	this.getAllVideos();
+  }
+
+  getAllVideos(){
+  	this.ss.getAllVideos().subscribe((data)=>{
+  		this.allvideos = data["hydra:member"];
+  		this.allvideos2 = this.allvideos;
+  	});
+  }
+
+  printSafeHtml(html){
+  	return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
+
+  showOneVideo(videoid){
+  	alert(videoid);
+  }
 
 }
